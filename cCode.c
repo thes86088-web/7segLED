f328474637b033bd5439f3a57bd034439f3f082c
durvesh <thes86088@gmail.com>
2026-01-28 22:15:28 +0530

    add LED/segment triggers for each valid digit

--- cCode.c
+++ cCode.c
@@ -4,43 +4,50 @@ int arr[size][size] = {0};
 
 void drawZero( )
 {
-    
+    drawL0(); drawL1(); drawL2(); drawL3(); drawL4();
+    drawL6();
 }
 void drawOne( )
 {
-    
+    drawL1(); drawL2();
 }
 void drawTwo( )
 {
-    
+    drawL0(); drawL1();
+    drawL3(); drawL4(); drawL5();
 }
 void drawThree( )
 {
-    
+    drawL0(); drawL1(); drawL2(); drawL3();
+    drawL5();
 }
 void drawFour( )
 {
-    
+    drawL1(); drawL2();
+    drawL5(); drawL6();
 }
 void drawFive()
 {
-    
+    drawL0();
+    drawL2(); drawL3(); 
+    drawL5(); drawL6();
 }
 void drawSix( )
 {
-    
+    drawL0(); drawL2(); drawL3(); drawL4(); drawL5(); drawL6();
 }
 void drawSeven( )
 {
-    
+    drawL0(); drawL1(); drawL2();
 }
 void drawEight( )
 {
-    
+    drawL0(); drawL1(); drawL2(); drawL3(); drawL4(); drawL5(); drawL6();
 }
 void drawNine( )
 {
-    
+    drawL0(); drawL1(); drawL2(); drawL3();
+    drawL5(); drawL6();
 }
 
 int main()
