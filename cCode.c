71e3565510e1cf327f17265e532546cb6d98b247
durvesh <thes86088@gmail.com>
2026-01-30 22:43:30 +0530

    change signatures of existing functions

--- cCode.c
+++ cCode.c
@@ -1,94 +1,97 @@
 #include <stdio.h>
 
-int i = 0; int j=0; int given = 8;
+int i = 0; int j=0; int given = 88;
 #define size 7
 int mid = size/2;
-int arr[size][size] = {0};
+int tens[size][size] = {0};
+int ones[size][size] = {0};
 
-void drawL0()
+void drawL0( int arr[] )
 {
     for(j=0; j<size; j++)
         arr[0][j] = 1;
 }
-void drawL1()
+void drawL1( int arr[] )
 {
     for(i=0; i<mid; i++)
         arr[i][size-1] = 1;
 }
-void drawL2()
+void drawL2( int arr[] )
 {
      for(i=0; i<mid; i++)
         arr[mid+i][size-1] = 1;   
 }
-void drawL3()
+void drawL3( int arr[] )
 {    
     for(j=0; j<size; j++)
         arr[size-1][j] = 1;
 }
-void drawL4()
+void drawL4( int arr[] )
 {
     for(i=0; i<mid; i++)
         arr[mid+i][0] = 1;   }
-void drawL5()
+
+void drawL5( int arr[] )
 {
     for(j=0; j<size; j++)
         arr[mid][j] = 1;    
 }
-void drawL6()
+void drawL6( int arr[] )
 {
     for(i=0; i<mid; i++)
         arr[i][0] = 1;    
 }
 
-void drawZero( )
+void drawZero( int arr[] )
 {
-    drawL0(); drawL1(); drawL2(); drawL3(); drawL4();
-    drawL6();
+    drawL0( int arr[] ); drawL1( int arr[] ); drawL2( int arr[] ); drawL3( int arr[] ); drawL4( int arr[] );
+    drawL6( int arr[] );
 }
-void drawOne( )
+void drawOne( int arr[] )
 {
-    drawL1(); drawL2();
+    drawL1( int arr[] ); drawL2( int arr[] );
 }
-void drawTwo( )
+void drawTwo( int arr[] )
 {
-    drawL0(); drawL1();
-    drawL3(); drawL4(); drawL5(); 
+    drawL0( int arr[] ); drawL1( int arr[] );
+    drawL3( int arr[] ); drawL4( int arr[] ); drawL5( int arr[] ); 
 }
-void drawThree( )
+void drawThree( int arr[] )
 {
-    drawL0(); drawL1(); drawL2(); drawL3();
-    drawL5();
+    drawL0( int arr[] ); drawL1( int arr[] ); drawL2( int arr[] ); drawL3( int arr[] );
+    drawL5( int arr[] );
 }
-void drawFour( )
+void drawFour( int arr[] )
 {
-    drawL1(); drawL2();
-    drawL5(); drawL6(); arr[size-1][size-1] = 1;
+    drawL1( int arr[] ); drawL2( int arr[] );
+    drawL5( int arr[] ); drawL6( int arr[] ); arr[size-1][size-1] = 1;
 }
-void drawFive()
+void drawFive( int arr[] )
 {
-    drawL0();
-    drawL2(); drawL3(); 
-    drawL5(); drawL6();
+    drawL0( int arr[] );
+    drawL2( int arr[] ); drawL3( int arr[] ); 
+    drawL5( int arr[] ); drawL6( int arr[] );
 }
-void drawSix( )
+void drawSix( int arr[] )
 {
-    drawL0(); drawL2(); drawL3(); drawL4(); drawL5(); drawL6();
+    drawL0( int arr[] ); drawL2( int arr[] ); drawL3( int arr[] ); drawL4( int arr[] ); drawL5( int arr[] ); drawL6( int arr[] );
 }
-void drawSeven( )
+void drawSeven( int arr[] )
 {
-    drawL0(); drawL1(); drawL2(); arr[size-1][size-1] = 1;
+    drawL0( int arr[] ); drawL1( int arr[] ); drawL2( int arr[] ); arr[size-1][size-1] = 1;
 }
-void drawEight( )
+void drawEight( int arr[] )
 {
-    drawL0(); drawL1(); drawL2(); drawL3(); drawL4(); drawL5(); drawL6();
+    drawL0( int arr[] ); drawL1( int arr[] ); drawL2( int arr[] ); drawL3( int arr[] );
+    drawL4( int arr[] ); drawL5( int arr[] ); drawL6( int arr[] );
 }
-void drawNine( )
+void drawNine( int arr[] )
 {
-    drawL0(); drawL1(); drawL2(); drawL3();
-    drawL5(); drawL6();
+    drawL0( int arr[] ); drawL1( int arr[] ); drawL2( int arr[] ); drawL3( int arr[] );
+    drawL5( int arr[] ); drawL6( int arr[] );
 }
 
-void printMat()
+void printMat( int arr[] )
 {
     for( i=0; i<size; i++ )
     {
@@ -101,7 +104,7 @@ void printMat()
     }
 }
 
-void initMat()
+void initMat( int arr[] )
 {
     for( i=0; i<size; i++ )
     {
@@ -113,36 +116,39 @@ void initMat()
 }
 
 
-void paintMat()
+void paintMat( int arr[], int given )
 {
     switch(given)
     {
-        case 0 : drawZero(); break;
-        case 1 : drawOne(); break;
-        case 2 : drawTwo(); break;
-        case 3 : drawThree(); break;
-        case 4 : drawFour(); break;
-        case 5 : drawFive(); break;
-        case 6 : drawSix(); break;
-        case 7 : drawSeven(); break;
-        case 8 : drawEight(); break;
-        case 9 : drawNine(); break;
-        default : drawEight();
+        case 0 : drawZero( int arr[] ); break;
+        case 1 : drawOne( int arr[] ); break;
+        case 2 : drawTwo( int arr[] ); break;
+        case 3 : drawThree( int arr[] ); break;
+        case 4 : drawFour( int arr[] ); break;
+        case 5 : drawFive( int arr[] ); break;
+        case 6 : drawSix( int arr[] ); break;
+        case 7 : drawSeven( int arr[] ); break;
+        case 8 : drawEight( int arr[] ); break;
+        case 9 : drawNine( int arr[] ); break;
+        default : drawEight( int arr[] );
     }
 }
 
 int main()
 {
     do{
-        if( given>9 || given<0 )
+        if( given>99 || given<0 )
         {
             printf("\n");
             printf("invalid input! please try again !"); printf("\n");
         }
-        printf("Enter a number 0-9 : ");
+        printf("Enter a number 0-99 : ");
         scanf( "%d", &given ); printf("\n");
     }
-    while( given>9 || given<0 );
+    while( given>99 || given<0 );
+
+    firstDigit = given/10;
+    secondDigit = given%10 ;
     
     initMat();
     paintMat();
